f8ec7cd5bf1e0ba5630109182ca2a7548cbf69fc
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2020-08-29 19:36:37 -0400

    git init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT  = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+const route = app.listen(PORT, () => {
+	console.log("server is up and running");
+});
+
